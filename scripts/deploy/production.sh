a2898574ed15711c7117da29d789454316ad5b1a
Yuriy Korpachan <korpachan@gmail.com>
2019-10-19 14:42:58 +0300

    fix

--- scripts/deploy/production.sh
+++ scripts/deploy/production.sh
@@ -23,6 +23,7 @@ export HISTIGNORE="";
 pass="";
 
 #remove previos container versions
+echo 'docker rmi $(docker images ${IMAGE_NAME} -q) -f';
 echo 'docker rmi $(docker images ${IMAGE_NAME} -q) -f' | ssh $PUB_HOST;
 #save new image on remote server
 docker save ${IMAGE_VERTION}.${BUILD_NUMBER} | ssh -C $PUB_HOST docker load;
